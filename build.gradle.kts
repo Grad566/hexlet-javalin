7cd5dc95dcd3e2574ba6691739d3d1077e905d8d
Denis Orekhov <3008denis@gmail.com>
2024-05-30 18:50:31 +0500

    Refactoring UserRepository

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,24 +18,22 @@ repositories {
 }
 
 dependencies {
-    // Версии зависимостей могут отличаться
-    // Здесь мы сразу подключаем зависимости,
-    // которые понадобятся во время обучения
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("org.apache.commons:commons-text:1.11.0")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation("io.javalin:javalin:6.1.3")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("io.javalin:javalin-rendering:6.1.3")
-    implementation("gg.jte:jte:3.1.9")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")
 
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
     compileOnly("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
-    testCompileOnly("org.projectlombok:lombok:1.18.32")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
-    // https://mvnrepository.com/artifact/org.apache.commons/commons-text
-    implementation("org.apache.commons:commons-text:1.12.0")
+
 
 
 }
--- src/main/java/org/example/hexlet/repository/UserRepository.java
+++ src/main/java/org/example/hexlet/repository/UserRepository.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.example.hexlet.model.User;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -11,19 +13,60 @@ import java.util.Optional;
 
 @Setter
 @Getter
-public class UserRepository {
-    @Getter
-    private static final List<User> users = new ArrayList<>();
+public class UserRepository extends BaseRepository{
+    public static void save(User user) throws SQLException {
+        var sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+        try (var conn = dataSource.getConnection();
+                var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getPassword());
+            preparedStatement.executeUpdate();
 
-    public static void save(User user) {
-        user.setId((long) users.size() + 1);
-        users.add(user);
+            var generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                user.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
     }
 
-    public static Optional<User> find(Long id) {
-        return users.stream()
-                .filter(u -> Objects.equals(u.getId(), id))
-                .findFirst();
+    public static Optional<User> find(Long id) throws SQLException {
+        var sql = "SELECT * FROM users WHERE id = ?";
+        try (var conn = dataSource.getConnection();
+                var stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, String.valueOf(id));
+            var resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                var name = resultSet.getString("name");
+                var email = resultSet.getString("email");
+                var password = resultSet.getString("password");
+                var user = new User(name, email, password);
+                user.setId(id);
+                return Optional.of(user);
+            }
+            return Optional.empty();
+        }
+    }
+
+    public static List<User> getEntities() throws SQLException {
+        var sql = "SELECT * FROM users";
+        try (var conn = dataSource.getConnection();
+                var stmt = conn.prepareStatement(sql)) {
+            var resultSet = stmt.executeQuery();
+            var result = new ArrayList<User>();
+            while (resultSet.next()) {
+                var id = resultSet.getString("id");
+                var name = resultSet.getString("name");
+                var email = resultSet.getString("email");
+                var password = resultSet.getString("password");
+                var user = new User(name, email, password);
+                user.setId(Long.valueOf(id));
+                result.add(user);
+            }
+            return result;
+        }
     }
 
 }
